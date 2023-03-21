69b8f85bc1a536dfc12f99c88135de3245986750
Ricardo <ricardo_slb_brites@live.ca>
2023-03-21 00:27:46 -0400

    Added recursive crawling

--- crawl.js
+++ crawl.js
@@ -12,48 +12,67 @@ const normalizeURL = (urlString) => {
   return `${host}${path}`;
 };
 
-const isValidURL = (urlString) => {
-  try {
-    return Boolean(new URL(urlString));
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-};
-
-const getURLsFromHTML = (html, baseURL) => {
+const getURLsFromHTML = (htmlBody, baseURL) => {
   const urls = [];
-  const dom = new JSDOM(html);
-  const aTags = dom.window.document.querySelectorAll("a");
-  aTags.forEach((aTag) => {
-    const urlString = aTag.href;
-    if (isValidURL(urlString)) {
-      urls.push(urlString);
-    } else if (urlString.startsWith("/")) {
-      urls.push(`${baseURL}${urlString}`);
+  const dom = new JSDOM(htmlBody);
+  const aElements = dom.window.document.querySelectorAll("a");
+  for (const aElement of aElements) {
+    if (aElement.href.slice(0, 1) === "/") {
+      try {
+        urls.push(new URL(aElement.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
     }
-  });
+  }
   return urls;
 };
 
 const crawlPage = async (baseURL, currentURL, pages) => {
-  try {
-    const response = await fetch(baseURL);
-    if (response.status > 399) {
-      console.error(`Got error with status ${response.status}.`);
-      return;
-    } else if (!response.headers.get("content-type").includes("text/html")) {
-      console.error(
-        `The response is not html, it is ${response.headers.get(
-          "content-type"
-        )}.`
-      );
-      return;
+  const base = new URL(baseURL);
+  const current = new URL(currentURL);
+  if (base.hostname !== current.hostname) {
+    return pages;
+  } else {
+    const normalizedCurrent = normalizeURL(currentURL);
+    if (pages[normalizedCurrent] > 0) {
+      pages[normalizedCurrent]++;
+      return pages;
     } else {
-      console.log(await response.text());
+      pages[normalizedCurrent] = 1;
+      let htmlBody;
+      try {
+        const response = await fetch(currentURL);
+        if (response.status > 399) {
+          console.error(`Got error with status ${response.status}.`);
+          return pages;
+        } else if (
+          !response.headers.get("content-type").includes("text/html")
+        ) {
+          console.error(
+            `The response is not html, it is ${response.headers.get(
+              "content-type"
+            )}.`
+          );
+          return pages;
+        } else {
+          htmlBody = await response.text();
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+      const pageUrls = getURLsFromHTML(htmlBody, baseURL);
+      for (const pageUrl of pageUrls) {
+        pages = await crawlPage(baseURL, pageUrl, pages);
+      }
+      return pages;
     }
-  } catch (error) {
-    console.log(error.message);
   }
 };
 
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const { crawlPage } = require("./crawl");
 
-function main() {
+async function main() {
   if (process.argv.length !== 3) {
     console.error("Must provide exactly one base url.");
     process.exit();
   } else {
     console.log(`The crawler has started at ${process.argv[2]}.`);
-    crawlPage(process.argv[2], {});
+    const pages = await crawlPage(process.argv[2], process.argv[2], {});
+    console.log(pages);
   }
 }
 main();
